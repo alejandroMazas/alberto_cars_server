2a107b373a1ab8fb0cb4559e755d4ac6a9c170a3
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-09-16 09:03:00 +0200

    cars routes fixed

--- routes/cars.routes.js
+++ routes/cars.routes.js
@@ -1,14 +1,11 @@
 const router = require("express").Router();
-const Car = require("../models/cars.model");
 const Cars = require('../models/cars.model')
 
 router.get('/cars', async (req, res) => {
 
     // Cars
     //     .find()
-    //     .then(allCars => {
-    //         res.json(allCars)
-    //     })
+    //     .then(allCars => res.json(allCars))
     //     .catch(err => res.status(500).json(err))
 
     try {
@@ -40,17 +37,17 @@ router.get('/cars/:id', async (req, res) => {
 
     const { id } = req.params
 
-    Cars
-        .findById(id)
-        .then(myCar => res.json(myCar))
-        .catch(err => res.status(500).json(err))
+    // Cars
+    //     .findById(id)
+    //     .then(myCar => res.json(myCar))
+    //     .catch(err => res.status(500).json(err))
 
-    // try {
-    //     const myCar = await Cars.findById(id)
-    //     res.json(myCar)
-    // } catch (error) {
-    //     res.status(500).json(err)
-    // }
+    try {
+        const myCar = await Cars.findById(id)
+        res.json(myCar)
+    } catch (error) {
+        res.status(500).json(err)
+    }
 
 })
 
@@ -58,6 +55,11 @@ router.put('/cars/:id/update', async (req, res) => {
 
     const { id } = req.params
     const { brand, model, image, generation, production } = req.body
+
+    // Cars
+    //     .findByIdAndUpdate(id, { brand, model, image, generation, production }, { new: true })
+    //     .then(updatedCar => res.json(updatedCar))
+    //     .catch(err => res.status(500).json(err))
 
     try {
         const updatedCar = await Cars.findByIdAndUpdate(id, { brand, model, image, generation, production }, { new: true })
@@ -71,19 +73,17 @@ router.delete('/cars/:id/delete', async (req, res) => {
 
     const { id } = req.params
 
-    Cars
-        .findByIdAndDelete(id)
-        .then(deletedCar => {
-            res.json(deletedCar)
-        })
-        .catch(err => res.status(500).json(err))
+    // Cars
+    //     .findByIdAndDelete(id)
+    //     .then(deletedCar => res.json(deletedCar))
+    //     .catch(err => res.status(500).json(err))
 
-    // try {
-    //     const deletedCar = await Cars.delete(id)
-    //     res.json(deletedCar)
-    // } catch (error) {
-    //     res.status(500).json(err)
-    // }
+    try {
+        const deletedCar = await Cars.findByIdAndDelete(id)
+        res.json(deletedCar)
+    } catch (error) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
