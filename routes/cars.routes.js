79b786bd87e0814c4b838596a3e4cda2aaadc350
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-09-16 08:55:05 +0200

    cars routes completed

--- routes/cars.routes.js
+++ routes/cars.routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
+const Car = require("../models/cars.model");
 const Cars = require('../models/cars.model')
 
-router.get('/', async (req, res) => {
+router.get('/cars', async (req, res) => {
 
     // Cars
     //     .find()
@@ -9,7 +10,7 @@ router.get('/', async (req, res) => {
     //         res.json(allCars)
     //     })
     //     .catch(err => res.status(500).json(err))
-    
+
     try {
         const allCars = await Cars.find();
         res.json(allCars);
@@ -33,6 +34,56 @@ router.post('/createCars', async (req, res) => {
     } catch (error) {
         res.status(500).json(err)
     }
+})
+
+router.get('/cars/:id', async (req, res) => {
+
+    const { id } = req.params
+
+    Cars
+        .findById(id)
+        .then(myCar => res.json(myCar))
+        .catch(err => res.status(500).json(err))
+
+    // try {
+    //     const myCar = await Cars.findById(id)
+    //     res.json(myCar)
+    // } catch (error) {
+    //     res.status(500).json(err)
+    // }
+
+})
+
+router.put('/cars/:id/update', async (req, res) => {
+
+    const { id } = req.params
+    const { brand, model, image, generation, production } = req.body
+
+    try {
+        const updatedCar = await Cars.findByIdAndUpdate(id, { brand, model, image, generation, production }, { new: true })
+        res.json(updatedCar)
+    } catch (error) {
+        res.status(500).json(err)
+    }
+})
+
+router.delete('/cars/:id/delete', async (req, res) => {
+
+    const { id } = req.params
+
+    Cars
+        .findByIdAndDelete(id)
+        .then(deletedCar => {
+            res.json(deletedCar)
+        })
+        .catch(err => res.status(500).json(err))
+
+    // try {
+    //     const deletedCar = await Cars.delete(id)
+    //     res.json(deletedCar)
+    // } catch (error) {
+    //     res.status(500).json(err)
+    // }
 })
 
 module.exports = router
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const carRoute = require('./cars.routes')
 
-router.use('/cars', carRoute)
+router.use('/', carRoute)
 
 module.exports = router;
