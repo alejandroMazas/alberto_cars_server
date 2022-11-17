d8ebad6f8c4c7b9803261000a02383d9ae5bd29b
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-11-17 23:23:39 +0100

    login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bcrypt = require('bcryptjs')
 const User = require('../models/user.model')
 const router = require('./cars.routes')
+const jwt = require('jsonwebtoken')
+
 
 const Router = express.Router()
 const saltRounds = 10
@@ -40,6 +42,42 @@ router.post('/signup', (req, res) => {
         .catch(err => {
             console.log(err)
             res.status(500).json({ message: "Internal server error" })
+        })
+
+})
+
+router.post('/login', (req, res) => {
+
+    if (email === "" || password === "") {
+        res.status(400).json({ message: 'Insert an email and a password' })
+        return
+    }
+
+    User
+        .findOne({ email })
+        .then((foundUser) => {
+
+            if (!foundUser) {
+                res.status(401).json({ message: 'User not found' })
+            }
+
+            if (bcrypt.compareSync(password, foundUser.password)) {
+
+                const { _id, username, email, role } = foundUser
+                const payload = { _id, email, username, role }
+
+                const authToken = jwt.sign(
+                    payload,
+                    process.env.TOKEN_SECRET,
+                    { algorithm: 'HS256', expiresIn: '10h' }
+                )
+
+                res.status(200).json({ authToken })
+            } else res.status(401).json({ message: 'Unable to authenticate the user' })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "Internal Server Error" })
         })
 
 })
